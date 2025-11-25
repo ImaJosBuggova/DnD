e3828c1eb555910a04c5b9d38f54c8b337bbe906
Roko Tomović <tomovicroko1@gmail.com>
2025-11-25 20:09:27 +0100

    .sln

--- Model/Event.cs
+++ Model/Event.cs
@@ -1,8 +1,13 @@
+using System.Drawing;
+
 namespace DnD.Model;
 
 public class Event
 {
     public int Id { get; set; }
-    
+
     public virtual List<Event> Events { get; set; } = [];
+
+    // Position of the event in the Editor
+    public Point Position { get; set; }
 }
